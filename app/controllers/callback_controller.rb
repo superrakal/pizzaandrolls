22ff82f6bbe4a591cb623139d229da53aac4d361
Egor <topolnyak012@gmail.com>
2015-08-19 10:36:36 +0300

    changes

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,7 +1,7 @@
 class CallbackController < ApplicationController
   def new_callback
     @text = "Клиент по имени  #{params[:name]} и с номером #{params[:number]} просит PIZZA&ROLLS перезвонить ему."
-    message = MainsmsApi::Message.new(sender: 'PIZZA AND ROLLS', message: @text, recipients: ['87055083705'])
+    message = MainsmsApi::Message.new(sender: 'PIZZA AND ROLLS', message: @text, recipients: ['87778514141'])
     message.deliver
     session[:callback] = true
     head :ok, content_type: 'text/html'
